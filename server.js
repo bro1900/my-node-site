bbb99da0e9158cdac727c4eb9d303b45e965d583
bro1900 <icuda8845@westada.org>
2026-03-24 08:44:23 -0600

    edits

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const app = express()
  
 const PORT = 3000
 
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
 })
-
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html')
