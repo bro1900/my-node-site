382524e20d902e16aa34167eeacba7f415713bb1
bro1900 <icuda8845@westada.org>
2026-03-30 09:53:34 -0600

    When this is over Im going to break each of your little fingers.

--- server.js
+++ server.js
@@ -1,7 +1,40 @@
 const express = require('express')
- 
+
+const nodemailer = require("nodemailer");
+
 const app = express()
- 
+
+// Middleware to read form data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "renctestudent@gmail.com",
+    pass: "qvkutajqvkfrmjkf"
+  }
+});
+app.post('/send', async (req, res) => {
+  const { fullName, email, reason, message } = req.body;
+  try {
+    await transporter.sendMail({
+      from: "isaaccuda@gmail.com",
+      to: "renctestudent@gmail.com",
+      subject: `Form: ${reason}`,
+      text: `Name: ${fullName}
+Email: ${email}
+Reason: ${reason}
+
+Message: ${message}`
+    });
+    res.send("Email sent!");
+  } catch (err) {
+    res.send("Error sending email");
+  }
+});
+
+
 const PORT = 3000
 
 const path = require('path');
@@ -9,7 +42,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`)
+  console.log(`Server running at http://localhost:${PORT}`)
 })
 
 app.get('/', (req, res) => {
@@ -27,5 +60,5 @@ app.get('/contact', (req, res) => {
 
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`)
+  console.log(`Server running at http://localhost:${PORT}`)
 })
